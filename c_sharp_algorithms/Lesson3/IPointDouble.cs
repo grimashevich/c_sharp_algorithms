135b2d9eb20d13994cb76899e21767e4b6c851a5
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-02-09 22:54:51 +0900

    Lesson 3

---
+++ c_sharp_algorithms/Lesson3/IPointDouble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_algorithms
+{
+    internal interface IPointDouble
+    {
+        double X1 { get; }
+        double Y1 { get; }
+        double X2 { get; }
+        double Y2 { get; }
+    }
+}
